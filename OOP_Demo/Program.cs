e08ff32c5d3b9c388eefe8df9493e22596c6ecfb
tuan <tuann14321@gmail.com>
2023-02-08 22:05:14 +0700

    ass1

---
+++ OOP_Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOP_Demo;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student students = new Student();
+        students.Id = 1;
+        students.Name = "Tuan";
+        Console.WriteLine(students.ToString());
+
+        Student student = new Student(2, "ok");
+        Console.WriteLine(student.ToString());
+    }
+}
---
+++ OOP_Demo/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Demo
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public Student(int id, string name)
+		{
+			Id = id;
+			Name = name;
+		}
+
+		public Student()
+		{
+		}
+
+		public override string? ToString()
+		{
+			return $"Id: {Id}, Name: {Name}";
+		}
+	}
+}
